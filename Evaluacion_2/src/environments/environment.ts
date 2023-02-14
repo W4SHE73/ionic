4bf8a3131bb974e1aecb99389af618f19fa80511
W4SHE73 <77137185+W4SHE73@users.noreply.github.com>
2023-02-14 01:37:28 -0300

    se borran las API Keys

--- Evaluacion_2/src/environments/environment.ts
+++ Evaluacion_2/src/environments/environment.ts
@@ -2,19 +2,19 @@
 export const environment = {
   production: false,
   //
-  API_key1: 'a41424a1ab454e70bdecd0713b235a81',
+  API_key1: '', //ingrece su Key 
   URL_base1: 'https://newsapi.org/v2/',
   //
-  API_key2: '1bf6934251eb5c37fc0f3eccf4b598f9',
-  URL_base2: 'https://api.openweathermap.org/data/2.5/',
+  API_key2: '',
+  URL_base2: 'https://api.openweathermap.org/data/2.5/', /ingrece su Key 
   //
   firebaseConfig : {
-    apiKey: "AIzaSyCW6-MXooIFkxS6ULvKv1wiElE-HkFAxow",
-    authDomain: "evaluacion2-67f42.firebaseapp.com",
-    projectId: "evaluacion2-67f42",
-    storageBucket: "evaluacion2-67f42.appspot.com",
-    messagingSenderId: "708399448475",
-    appId: "1:708399448475:web:3c0906948adfe7a35ee13e",
-    measurementId: "G-ZHSSQECVLM"
+    apiKey: " ",
+    authDomain: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
   }
 };
